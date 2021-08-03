d37186901ac66eec1d303ff56513eb525e43a934
Phantomn <tmdvyr123@gmail.com>
2021-08-02 23:48:33 -0700

    update file

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-qemu-system-mipsel -M malta -kernel vmlinux-3.2.0-4-4kc-malta -hda debian_wheezy_mipsel_standard.qcow2 \
+qemu-system-mipsel -cdrom debian-9.1.0-mipsel-netinst.iso \
+		   -hda hda.qcow -M malta -kernel vmlinux-4.9.0-13-4kc-malta \
+		   -boot d -initrd initrd.gz -smp 4 -m 1G \
  		   -append "root=/dev/sda1 console=tty0" \
 		   -netdev user,id=net0,hostfwd=tcp::2022-:22,hostfwd=tcp::2080-:80 -device virtio-net,netdev=net0 \
 		   -nographic
