388687c4e69e220dd0d353cc01c31f80df1212e8
Phantomn <tmdvyr123@gmail.com>
2021-08-04 00:22:42 -0700

    update file

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-qemu-system-mipsel -cdrom debian-9.1.0-mipsel-netinst.iso \
-		   -hda hda.qcow -M malta -kernel vmlinux-4.9.0-13-4kc-malta \
-		   -boot d -initrd initrd.gz -smp 4 -m 1G \
- 		   -append "root=/dev/sda1 console=tty0" \
-		   -netdev user,id=net0,hostfwd=tcp::2022-:22,hostfwd=tcp::2080-:80 -device virtio-net,netdev=net0 \
-		   -nographic
+qemu-system-mipsel -hda ./stretch.img -M malta -kernel ./vmlinux-4.9.0-13-4kc-malta \
+		   -initrd ./initrd.gz \
+ 		   -append "quite root=/dev/sda1 console=tty0 nokaslr" -nographic 
+#		   -netdev user,id=net0,hostfwd=tcp::2022-:22,hostfwd=tcp::2080-:80 -device virtio-net,netdev=net0 \
+#		   -nographic
+#qemu-system-x86_64 -kernel bzImage -append "quiet console=ttyS0 root=/dev/sda" -hda ./stretch.img -nographic -m 2G -smp 2 -s
+
