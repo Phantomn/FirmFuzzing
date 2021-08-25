fe526c05e4dfefc0c3ae60044232ff94cb5801bc
Phantomn <tmdvyr123@gmail.com>
2021-08-24 23:18:09 -0700

    add set.c

---
+++ set.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+extern char** environ;
+
+void main(int argc, char* argv[]){
+	char s[0x60000] = {};
+	char q[0x60000] = {};
+
+	sprintf(s, "tmenu=");
+	scanf("%1024s", q);
+	strcat(s,q);
+	strcat(s, "&smenu=");
+	scanf("%1024s", q);
+	strcat(s,q);
+	printf("%s\n",s);
+
+	setenv("QUERY_STRING", s, 1);
+	setenv("REQUEST_METHOD", "GET", 1);	
+	execv("/cgibin/timepro.cgi", NULL, environ);
+
+}
