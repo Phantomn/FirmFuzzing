7a0801645b22438a74794821e9d275a8aea891a0
Phantomn <tmdvyr123@gmail.com>
2021-08-26 02:42:20 -0700

    update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -1,19 +1,29 @@
 import re
+import sys
+import random
+import os
 
-f = open("./cgibin/timepro.cgi", "rb")
+f = open("./squashfs-root/cgibin/timepro.cgi", "rb")
 read = f.read()
 
 regex = re.findall(r'cgi\?[a-zA-Z]+=[a-zA-Z_-]+&[a-zA-Z]+=[a-zA-Z_-]+', read)
 tmenu = []
 smenu = []
-#cgi?page=url_redirect&subpage=frame
 for i in regex:
-	data = i.replace(re.match(r'cgi\?[a-zA-z]+=',i).group(), "")
-	tmenu.append(re.match(r'[a-zA-Z_-]+',data).group())
-	data2 = re.search(r'=[a-zA-Z_-]+',data).group()
-	smenu.append(data2.replace("=", ""))
+        data = i.replace(re.match(r'cgi\?[a-zA-z]+=',i).group(), "")
+        tmenu.append(re.match(r'[a-zA-Z_-]+',data).group())
+        data2 = re.search(r'=[a-zA-Z_-]+',data).group()
+        smenu.append(data2.replace("=", ""))
 
-print tmenu
-print
-print
-print smenu
+s = ""
+q = ""
+s += "tmenu="
+q = raw_input()
+s = s + q
+s += "&smenu="
+q = raw_input()
+s = s + q
+print s
+os.environ["QUERY_STRING"] = s
+os.environ["REQUEST_METHOD"] = "POST"
+os.execve("./squashfs-root/cgibin/timepro.cgi", sys.argv, os.environ)
